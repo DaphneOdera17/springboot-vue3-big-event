7bad1ba43264fcb8f8dc8b19d805dec15d8cbf6c
DaphneOdera17 <2944006086@qq.com>
2025-01-28 14:56:34 +0800

    注册登录前后端交接

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:8080';
+import {ElMessage} from "element-plus";
+
+// http://localhost:5173/api
+const baseURL = '/api';
 const instance = axios.create({baseURL});
 
+// 相应拦截器
 instance.interceptors.response.use(
     result=>{
-        return result.data;
+        if(result.data.code === 0) {
+            return result.data;
+        }
+
+        // 操作失败
+        // alert(result.data.msg ? result.data.msg : "服务异常");
+        ElMessage.error(result.data.message ? result.data.message : "服务异常")
+        // 将异步操作的状态转换为失败
+        return Promise.reject(result.data);
     },
     err=>{
         alert('服务异常');
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,4 +15,17 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  server: {
+    proxy: {
+      // 获取路径中包含 /api 的请求
+      '/api': {
+        // 后台服务所在的源
+        target: 'http://localhost:8080',
+        changeOrigin: true, // 修改源
+        // 将 /api 替换为 ''
+        rewrite: (path)=>path.replace(/^\/api/,"")
+      }
+    }
+  }
 })
