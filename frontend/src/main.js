015c1df0312f6b91aea1f5d4b9ea906f12914310
DaphneOdera17 <2944006086@qq.com>
2025-01-29 15:03:45 +0800

    完善登录以及主界面

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,18 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import router from '@/router'
+import {createPinia} from "pinia";
+import {createPersistedState} from "pinia-persistedstate-plugin";
+
 import App from './App.vue'
 
+const pinia = createPinia()
 const app = createApp(App);
-app.use(ElementPlus).mount('#app');
+const persist = createPersistedState()
+pinia.use(persist)
+
+app.use(pinia).
+    use(router).
+    use(ElementPlus).
+    mount('#app');
--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -6,7 +6,28 @@ import {ElMessage} from "element-plus";
 const baseURL = '/api';
 const instance = axios.create({baseURL});
 
-// 相应拦截器
+import {useTokenStore} from "@/stores/token.js";
+// 添加请求拦截器
+instance.interceptors.request.use(
+    (config)=>{
+        // 请求前的回调
+        // 添加 token
+        const tokenStore = useTokenStore()
+        // 判断有没有 token
+        if(tokenStore.token) {
+            config.headers.Authorization = tokenStore.token;
+        }
+        return config
+    },
+    (err)=>{
+        // 请求错误的回调
+        Promise.reject(err)
+    }
+)
+
+import router from '@/router'
+
+// 添加响应拦截器
 instance.interceptors.response.use(
     result=>{
         if(result.data.code === 0) {
@@ -20,7 +41,14 @@ instance.interceptors.response.use(
         return Promise.reject(result.data);
     },
     err=>{
-        alert('服务异常');
+        // 判断响应状态码，如果为 401 则说明未登录
+        if(err.response.status === 401){
+            ElMessage.error('请先登录')
+            router.push('/login')
+        } else {
+            ElMessage.error('服务异常');
+        }
+
         return Promise.reject(err);
     }
 )
